7b8432bb1f48a9b87fb2663f0d6726347e5ba977
InSeong-So <goflvhxj2547@gmail.com>
2023-09-02 16:50:18 +0900

    feat: API 연동과 함께 ErrorBoundary를 추가하고 기존에 있던 라우트 코드를 대체합니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import type { Component } from 'solid-js';
-import { Route, Router, Routes } from '@solidjs/router';
-import ArticlePage from '@/pages/article/[slug]';
-import EditorPage from '@/pages/editor';
-import EditArticlePage from '@/pages/editor/[slug]';
-import HomePage from '@/pages/home';
-import LoginPage from '@/pages/login';
-import ProfilePage from '@/pages/profile/[username]';
-import FavoriteProfilePage from '@/pages/profile/[username]/favorites';
-import RegisterPage from '@/pages/register';
-import SettingPage from '@/pages/settings';
+import { type Component, ErrorBoundary } from 'solid-js';
+import { Routes } from './routes';
 
 const App: Component = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" component={HomePage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/register" component={RegisterPage} />
-        <Route path="/settings" component={SettingPage} />
-        <Route path="/profile/:username" component={ProfilePage}>
-          <Route path="/favorite" component={FavoriteProfilePage} />
-        </Route>
-        <Route path="/article/:slug" component={ArticlePage} />
-        <Route path="/editor" component={EditorPage}>
-          <Route path="/:slug" component={EditArticlePage} />
-        </Route>
-      </Routes>
-    </Router>
+    <ErrorBoundary fallback={<div>저런!! 😳</div>}>
+      <Routes />
+    </ErrorBoundary>
   );
 };
 
